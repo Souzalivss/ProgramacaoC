62d3a5909b118c0e814b87ef2463989cc02e38c8
Souzalivss <souzalivia8412@gmail.com>
2026-06-15 08:12:01 -0300

    desafio.If.Else.c

---
+++ testes/testes.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int idade = 19;
+
+   if (idade >= 18) {
+    printf("voce é maior de idade");
+   } else {
+    printf("voce é menor de idade");
+   }
+   
+   return 0;
+   
+}
